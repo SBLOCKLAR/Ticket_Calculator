574999bc74a707a9c46a50816c0a0ab3f0ec4726
Sarah Beth Locklar <110067009+SBLOCKLAR@users.noreply.github.com>
2023-03-12 20:11:29 -0400

    Completed V1
    Added all styles, elements and JS.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # TICKETSCalc
  
+BG image - Photo by <a href="https://unsplash.com/@fempreneurstyledstock?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Leone Venter</a> on <a href="https://unsplash.com/photos/VieM9BdZKFo?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+  
---
+++ app.js
@@ -0,0 +1,41 @@
+const avgMonthlyInput = document.querySelector('input')
+// const avgMonthly = avgMonthlyInput.value;
+const annualEl = document.querySelector('#grossAnnual');
+const visaMCEl = document.querySelector('#visaMC');
+const discoverEl = document.querySelector('#discover');
+const amexEl = document.querySelector('#amex');
+
+
+// First pull Annual
+function grossAnnual(avgMonthly) {
+    let grossAnnualResult = avgMonthly * 12;
+    annualEl.innerText = '$' + parseInt(grossAnnualResult);
+}
+
+// function visaMC(grossAnnual)
+function visaMC(avgMonthly) {
+    let visaMCResult = (avgMonthly * 12) * 0.70;
+    visaMCEl.innerText = '$' + parseInt(visaMCResult);
+}
+
+// function Discover
+function discover(avgMonthly) {
+    let discoverResult = (avgMonthly * 12) * 0.05;
+    discoverEl.innerText = '$' + parseInt(discoverResult);
+}
+
+// function AMEX
+function amex(avgMonthly) {
+    let amexResult = (avgMonthly * 12) * 0.25;
+    amexEl.innerText = '$' + parseInt(amexResult);
+}
+
+//  Then insert in appropriate fields
+avgMonthlyInput.addEventListener('input', function () {
+    const avgMonthly = avgMonthlyInput.value;
+    grossAnnual(avgMonthly);
+    visaMC(avgMonthly);
+    discover(avgMonthly);
+    amex(avgMonthly)
+});
+
